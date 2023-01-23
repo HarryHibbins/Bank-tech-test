7377465197403fa570c12607d633965c95a778a4
Harry Hibbins <harryhibbins11@gmail.com>
2023-01-23 11:17:20 +0000

    Depoist a value greater than 0

--- account.js
+++ account.js
@@ -6,6 +6,14 @@ class account {
   GetBalance = () => {
     return this.balance;
   }
+
+  Deposit = (amount) => {
+    if (amount <= 0){
+      throw new Error('Number must be greater than 0')
+    }
+    
+    this.balance += amount;
+  }
 }
 
 module.exports = account
--- account.test.js
+++ account.test.js
@@ -1,9 +1,24 @@
+const account = require('./account');
 const Account = require('./account')
 
 describe(Account, () => {
   it("Has a balance", () => {
-    account = new Account;
-
+    const account = new Account;
+    
     expect(account.GetBalance()).toBe(0)
+  })
+  it("Balance can be increased by depositing", () => {
+    const account = new Account;
+    
+    account.Deposit(1000);
+    expect(account.GetBalance()).toBe(1000)
+  })
+  it("Only a postive number can be deposited", () => {
+    const account = new Account;
+    
+    expect(() => {account.Deposit(-1000)}).toThrow(Error)
+    expect(() => {account.Deposit(-1000)}).toThrow("Number must be greater than 0")
+    expect(() => {account.Deposit(0)}).toThrow(Error)
+    expect(() => {account.Deposit(0)}).toThrow("Number must be greater than 0")
   })
 })
