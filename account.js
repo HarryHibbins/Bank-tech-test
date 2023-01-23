196b83bc8a37f481fb6ea09b98f84ec26e934852
Harry Hibbins <harryhibbins11@gmail.com>
2023-01-23 11:01:49 +0000

    Account has a balance

--- account.js
+++ account.js
@@ -1,5 +1,11 @@
 class account {
+  constructor(){
+    this.balance = 0;
+  }
 
+  GetBalance = () => {
+    return this.balance;
+  }
 }
 
 module.exports = account
--- account.test.js
+++ account.test.js
@@ -2,6 +2,8 @@ const Account = require('./account')
 
 describe(Account, () => {
   it("Has a balance", () => {
-    
+    account = new Account;
+
+    expect(account.GetBalance()).toBe(0)
   })
 })
