a91b08201f10ea910c1b2e9cd2ea765327e1a17c
Harry Hibbins <harryhibbins11@gmail.com>
2023-01-23 14:06:45 +0000

    deposits and withdrawals have date, amount, balance, and type. And are stored in an array called transactions

--- account.js
+++ account.js
@@ -1,6 +1,7 @@
 class account {
   constructor(){
     this.balance = 0;
+    this.transactions = []
   }
 
   GetBalance = () => {
@@ -8,11 +9,51 @@ class account {
   }
 
   Deposit = (amount) => {
+    this.AmountValidation(amount)
+
+    this.balance += amount;
+    const date = this.formatDate(new Date)
+    const deposit = {amount: amount, date: date, balance: this.balance, type: "DEPOSIT"}
+    this.transactions.push(deposit)
+    return deposit
+
+  }
+  Withdraw = (amount) => {
+    this.AmountValidation(amount)
+    if (amount > this.GetBalance()){
+      throw new Error('Insufficient funds')
+    }
+    const date = this.formatDate(new Date)
+    this.balance -= amount;
+    const withdraw = {amount: amount, date: date, balance: this.balance, type: "WITHDRAW"};
+    this.transactions.push(withdraw)
+    return withdraw;
+
+  }
+
+  GetTransactions(){
+    return this.transactions;
+  }
+  
+
+
+
+  AmountValidation = (amount) => {
     if (amount <= 0){
       throw new Error('Number must be greater than 0')
     }
-    
-    this.balance += amount;
+  }
+
+  formatDate = (date) => {
+    const day = date.getDate()
+    let month = date.getMonth() +1
+    const year = date.getFullYear()
+    if (month < 10){
+      month = "0"+month
+    }
+
+    const date_ = `${day}/${month}/${year}`;
+    return date_;
   }
 }
 
--- account.test.js
+++ account.test.js
@@ -1,4 +1,3 @@
-const account = require('./account');
 const Account = require('./account')
 
 describe(Account, () => {
@@ -10,15 +9,77 @@ describe(Account, () => {
   it("Balance can be increased by depositing", () => {
     const account = new Account;
     
-    account.Deposit(1000);
+    const deposit = account.Deposit(1000);
+    expect(deposit.amount).toBe(1000)
+    expect(deposit.balance).toBe(1000)
     expect(account.GetBalance()).toBe(1000)
   })
-  it("Only a postive number can be deposited", () => {
+  it("Only a number greater than 0 can be deposited", () => {
     const account = new Account;
     
     expect(() => {account.Deposit(-1000)}).toThrow(Error)
     expect(() => {account.Deposit(-1000)}).toThrow("Number must be greater than 0")
     expect(() => {account.Deposit(0)}).toThrow(Error)
     expect(() => {account.Deposit(0)}).toThrow("Number must be greater than 0")
+
+    expect(account.GetBalance()).toBe(0)
+  })
+  it("Balance can be decreased by withdrawing", () => {
+    const account = new Account();
+    
+    account.Deposit(1000)
+    const withdraw = account.Withdraw(500)
+    expect(withdraw.amount).toBe(500)
+    expect(withdraw.balance).toBe(500)
+    expect(account.GetBalance()).toBe(500)
+  })
+  it("Only a number greater than 0 can be withdrawn", () => {
+    const account = new Account;
+    
+    expect(() => {account.Withdraw(-1000)}).toThrow(Error)
+    expect(() => {account.Withdraw(-1000)}).toThrow("Number must be greater than 0")
+    expect(() => {account.Withdraw(0)}).toThrow(Error)
+    expect(() => {account.Withdraw(0)}).toThrow("Number must be greater than 0")
+
+    expect(account.GetBalance()).toBe(0)
+  })
+  it("Amount must be greater than balance to be withdrawn", () => {
+    const account = new Account;
+    
+    expect(() => {account.Withdraw(500)}).toThrow(Error)
+    expect(() => {account.Withdraw(500)}).toThrow("Insufficient funds")
+    
+    expect(account.GetBalance()).toBe(0)
+  })
+  it("Adds deposits to a list of transactions", () => {
+    const account = new Account;
+    const deposit = account.Deposit(1000)
+    expect(account.GetTransactions()).toEqual([deposit])
+    expect(account.GetTransactions()[0].amount).toBe(1000)
+    expect(account.GetTransactions()[0].type).toBe("DEPOSIT")
+
+  })
+  it("Adds withdrawals to a list of transactions", () => {
+    const account = new Account;
+    const deposit = account.Deposit(1000)
+    const withdraw = account.Withdraw(250)
+    expect(account.GetTransactions()).toEqual([deposit, withdraw])
+    expect(account.GetTransactions()[1].amount).toBe(250)
+    expect(account.GetTransactions()[1].balance).toBe(750)
+    expect(account.GetTransactions()[1].type).toBe("WITHDRAW")
+
+  })
+  it("Adds deposits and withdrawals to a list of transactions", () => {
+    const account = new Account;
+    const deposit = account.Deposit(1000)
+    const withdraw = account.Withdraw(250)
+    expect(account.GetTransactions()).toEqual([deposit, withdraw])
+    expect(account.GetTransactions()[0].amount).toBe(1000)
+    expect(account.GetTransactions()[0].type).toBe("DEPOSIT")
+    expect(account.GetTransactions()[0].balance).toBe(1000)
+    expect(account.GetTransactions()[1].amount).toBe(250)
+    expect(account.GetTransactions()[1].balance).toBe(750)
+    expect(account.GetTransactions()[1].type).toBe("WITHDRAW")
+
   })
 })
